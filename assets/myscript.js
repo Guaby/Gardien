3f6623bf453d00ac6fff3dccbce54cd2a89a1c74
Gabriel Rosario <gabrielrosario@Gabriels-MacBook-Pro.local>
2018-01-09 19:51:54 -0400

    180109 project changes

--- assets/myscript.js
+++ assets/myscript.js
@@ -1,1 +1,10 @@
 console.log('my script is runing');
+
+// Make nav clone
+
+// let headerParent = $('div[data-section-id="header"]');
+// let header = $('header');
+
+// console.log(header);
+
+// header.clone(true, true).appendTo(headerParent).addClass('fixed-header');
